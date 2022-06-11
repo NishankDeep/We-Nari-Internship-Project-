2313d9e8d630753d82da6b8698fab7a4a72cce6f
Nishank <nishankdeep@gamil.com>
2022-06-12 00:10:16 +0530

    added the form folder containing customization form

---
+++ form/index.js
@@ -0,0 +1,16 @@
+// removing the outlining of the box
+var inp1=document.querySelector("#mail");
+inp1.addEventListener("click",function(){
+    inp1.classList.add("rmvBxShadow")
+})
+
+var inp2=document.querySelector("#disc");
+inp2.addEventListener("click",function(){
+    inp2.classList.add("rmvBxShadow")
+})
+
+var inp3=document.querySelector("#attach");
+inp3.addEventListener("click",function(){
+    inp3.classList.add("rmvBxShadow")
+})
+
