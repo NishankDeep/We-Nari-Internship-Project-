3af652fb041b3c5ac4804bd58cba6970f9bc2a2e
Prateek Kumar Mishra <pr.prateekmishra@gmail.com>
2022-10-30 19:23:57 +0530

    Revert "modification"
    This reverts commit 8fcbe3b44b75dfe1655992485defb4e4eef9b2c1.

--- public/js/script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('scroll', function () {
-
-    let nav = document.querySelector('nav');
-    nav.classList.toggle('scrolling-active', window.scrollY > 0);
-});
-document.documentElement.style.setProperty('--nav-height', document.querySelector('nav').offsetHeight);
-
-document.documentElement.style.setProperty('--container-height', document.getElementById('form-cnt').offsetHeight + 20);
