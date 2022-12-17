8c3a10d9cb9c0d3ac76728a2aaafa4b78b884dd2
Nishank <nishankdeep@gamil.com>
2022-12-17 16:52:16 +0530

    corrected fault in footer

--- controller/shop.js
+++ controller/shop.js
@@ -5,7 +5,7 @@ const User = require('../models/user.js')
 const deliveryDetails = require('../models/deliveryDetails')
 
 exports.home = (req, res) => {
-
+    // res.render("new");
     Product.find((err, product_data) => {
 
         let user_name = '';
