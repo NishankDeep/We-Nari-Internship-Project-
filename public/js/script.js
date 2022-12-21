b966ab515b457f0f8e4800dffe9ef5308213c9a3
Prateek Kumar Mishra <pr.prateekmishra@gmail.com>
2022-12-21 17:26:36 +0530

    added show password option and bug fixes

--- public/js/script.js
+++ public/js/script.js
@@ -6,3 +6,11 @@ function toggle_bg() {
 
     nav.classList.toggle('scrolling-active', window.scrollY > 0);
 }
+function show() {
+
+    let pass = document.getElementById('password')
+
+    if (pass.type == 'password') pass.type = 'text'
+
+    else pass.type = 'password'
+}
