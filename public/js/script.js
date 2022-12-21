463ef59e5d7f97ddbb62d505ebc84df1ec656825
Prateek Kumar Mishra <pr.prateekmishra@gmail.com>
2022-12-21 17:28:21 +0530

    added show password option has some modifications

--- public/js/script.js
+++ public/js/script.js
@@ -14,3 +14,19 @@ function show() {
 
     else pass.type = 'password'
 }
+function show1() {
+
+    let pass = document.getElementById('password1')
+
+    if (pass.type == 'password') pass.type = 'text'
+
+    else pass.type = 'password'
+}
+function show2() {
+
+    let pass = document.getElementById('password2')
+
+    if (pass.type == 'password') pass.type = 'text'
+
+    else pass.type = 'password'
+}
